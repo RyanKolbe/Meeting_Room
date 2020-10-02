825626d2f5c8aaf02ac0e828e3ca23fb0de59856
RyanKolbe <31966635+RyanKolbe@users.noreply.github.com>
2020-10-02 23:25:06 +0200

    Final Commit - Unable to complete Booking section.

---
+++ app/src/main/java/com/ryankolbe/meetingroom/BookingActivities/CreateBookingActivity.java
@@ -0,0 +1,16 @@
+package com.ryankolbe.meetingroom.BookingActivities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ryankolbe.meetingroom.R;
+
+public class CreateBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_booking);
+    }
+}
